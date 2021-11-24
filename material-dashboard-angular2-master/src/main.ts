f7f6588d58d1db059820f39617daa6a94ce664e5
alereyes1598 <160300139@ucaribe.edu.mx>
2021-11-24 01:14:57 -0500

    Agrega cambios finales en el apartado de inmuebles

--- material-dashboard-angular2-master/src/main.ts
+++ material-dashboard-angular2-master/src/main.ts
@@ -1,14 +1,13 @@
 /*!
 
 =========================================================
-* Material Dashboard Angular - v2.6.0
+* Hydra Builder Dashboard - v2.6.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
 * Copyright 2021 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular2/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
 
 =========================================================
 
